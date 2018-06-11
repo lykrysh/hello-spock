8a7c9cd3f70d7bccc411a4f5c0af0ecf70eab934
lykrysh <lykrysh@email.com>
2018-06-10 22:25:24 -0700

    Sort out data types

--- src/Main.hs
+++ src/Main.hs
@@ -7,23 +7,33 @@ import Web.Spock.Lucid (lucid)
 import Lucid
 import qualified Database.PostgreSQL.Simple as Pg
 import qualified Data.Configurator as Cf
+import Data.IORef (IORef, newIORef)
+import Data.Int (Int64)
+
+type MyM = SpockM MyDb MySess MyState ()
+type MyDb = Pg.Connection
+data MySess = EmptySession
+data MyState =  MyState (IORef Int)
 
 main :: IO ()
 main = do
   connInfo <- myConnInfo "pg.cfg"
   conn <- Pg.connect connInfo
-  cfg <- defaultSpockCfg () (PCConn (mkConn connInfo)) ()
+  createTable conn
+
+  ref <- newIORef 0
+  cfg <- defaultSpockCfg EmptySession (PCConn (mkConn connInfo)) (MyState ref)
   runSpock 8080 (spock cfg app)
 
-app :: SpockM Pg.Connection () state ()
+app :: MyM
 app = do
   get root helloAction
 
-helloAction :: SpockAction db sess state ()
+helloAction :: SpockAction MyDb MySess MyState ()
 helloAction = do
   helloInHtml
 
-helloInHtml :: SpockActionCtx ctx db sess state ()
+helloInHtml :: SpockActionCtx ctx MyDb MySess MyState ()
 helloInHtml = lucid $ do
   html_ $ do
     head_ (title_ "Hello")
@@ -35,6 +45,10 @@ helloInHtml = lucid $ do
         a_ [href_ "http://haskell.org"] "doggy"
         " !!!"
 
+createTable :: MyDb -> IO Int64
+createTable c =
+  Pg.execute_ c "create table if not exists guest (name varchar(80));"
+
 myConnInfo :: FilePath -> IO Pg.ConnectInfo
 myConnInfo path = do
   pgCfg <- Cf.load $ [Cf.Required path]
@@ -44,7 +58,7 @@ myConnInfo path = do
                  <*> Cf.require pgCfg "password"
                  <*> Cf.require pgCfg "db"
 
-mkConn :: Pg.ConnectInfo -> ConnBuilder Pg.Connection
+mkConn :: Pg.ConnectInfo -> ConnBuilder MyDb
 mkConn connInfo =
   ConnBuilder { cb_createConn = Pg.connect connInfo
               , cb_destroyConn = Pg.close
