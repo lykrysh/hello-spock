3101946fb4b947547d0a4c11e4f0d89c59c66156
lykrysh <lykrysh@email.com>
2018-06-10 20:32:31 -0700

    Add movies using post requests

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 module Main where
 
 import Web.Spock
@@ -6,34 +7,94 @@ import Web.Spock.Config
 import Web.Spock.Lucid (lucid)
 import Lucid
 import qualified Database.PostgreSQL.Simple as Pg
+import qualified Database.PostgreSQL.Simple.FromRow as Pg
+import qualified Database.PostgreSQL.Simple.ToRow as Pg
+import qualified Database.PostgreSQL.Simple.ToField as Pg
 import qualified Data.Configurator as Cf
+import Data.Text (Text, pack)
+import Data.IORef (IORef, newIORef, atomicModifyIORef')
+import Data.Int (Int64)
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (liftIO)
+import System.Random (randomRIO)
+import BasePrelude
+
+type Server a = SpockM () () ServerState ()
+newtype ServerState = ServerState { movies :: IORef [Movie]}
+data Movie = Movie { title :: Text, signature :: Text, _id :: Integer }
 
 main :: IO ()
 main = do
   connInfo <- myConnInfo "pg.cfg"
   conn <- Pg.connect connInfo
-  cfg <- defaultSpockCfg () (PCConn (mkConn connInfo)) ()
-  runSpock 8080 (spock cfg app)
 
-app :: SpockM Pg.Connection () state ()
-app = do
-  get root helloAction
+  st <- ServerState <$>
+    newIORef [ Movie "movie one" "140592938796" 1
+             , Movie "movie two" "p02924787568" 2
+             ]
 
-helloAction :: SpockAction db sess state ()
-helloAction = do
-  helloInHtml
+  cfg <- defaultSpockCfg () (PCConn (mkConn connInfo)) st
+  runSpock 8080 (spock cfg myapp)
 
-helloInHtml :: SpockActionCtx ctx db sess state ()
-helloInHtml = lucid $ do
+myapp :: SpockM Pg.Connection () ServerState ()
+myapp = do
+  get root $ do
+    movies' <- runQuery (\conn -> allMovies conn)
+    lucid $ do
+      renderHtml movies'
+      putMoviesForm
+  post root $ do
+    title <- param' "title"
+    signature <- param' "signature"
+    _id <- liftIO $ randomRIO (1,10)
+    _ <- runQuery $ insertMovie $ Movie title signature _id
+    moviesRef <- movies <$> getState
+    liftIO $ atomicModifyIORef' moviesRef $ \movies ->
+      (movies <> [Movie title signature _id], ())
+    redirect "/"
+
+allMovies :: Pg.Connection -> IO [Movie]
+allMovies c = Pg.query_ c "select title, signature, _id from movies"
+
+instance Pg.FromRow Movie where
+  fromRow = Movie <$> Pg.field <*> Pg.field <*> Pg.field
+
+renderHtml :: [Movie] -> Html ()
+renderHtml notes' = do
   html_ $ do
-    head_ (title_ "Hello")
+    head_ (title_ "Movies")
     body_ $ do
-      h1_ "Hello!"
-      p_ "Hello! Lucid"
+      h1_ "Movies!"
       p_ $ do
         "I love "
         a_ [href_ "http://haskell.org"] "doggy"
         " !!!"
+      ul_ $ forM_ notes' $ \movie -> li_ $ do
+        toHtml (title movie)
+        ": "
+        toHtml (signature movie)
+
+putMoviesForm :: Html ()
+putMoviesForm = do
+  h1_ "Add new movie"
+  form_ [method_ "post"] $ do
+    label_ $ do
+      "Title: "
+      input_ [name_ "title"]
+    label_ $ do
+      "Signature: "
+      textarea_ [name_ "signature"] ""
+    input_ [type_ "submit", value_ "Add movie"]
+
+insertMovie :: Movie -> Pg.Connection -> IO Int64
+insertMovie n conn =
+  Pg.executeMany
+    conn
+    "insert into movies (title, signature, _id) values (?, ?, ?)"
+    [n]
+
+instance Pg.ToRow Movie where
+  toRow n = Pg.toField <$> [title n, signature n, pack $ show $_id n]
 
 myConnInfo :: FilePath -> IO Pg.ConnectInfo
 myConnInfo path = do
