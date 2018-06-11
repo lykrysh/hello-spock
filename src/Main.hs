9cd2be526fa47545abef373d56fc381c9b0c658b
lykrysh <lykrysh@email.com>
2018-06-11 00:20:00 -0700

    Create new page via get request

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,10 @@ import Web.Spock.Lucid (lucid)
 import Lucid
 import qualified Database.PostgreSQL.Simple as Pg
 import qualified Data.Configurator as Cf
-import Data.IORef (IORef, newIORef)
+import Data.IORef (IORef, newIORef, atomicModifyIORef')
 import Data.Int (Int64)
+import qualified Data.Text as T
+import Control.Monad.IO.Class (liftIO)
 
 type MyM = SpockM MyDb MySess MyState ()
 type MyDb = Pg.Connection
@@ -27,16 +29,28 @@ main = do
 
 app :: MyM
 app = do
-  get root helloAction
+  get root $ do
+    (MyState ref) <- getState
+    visitNum <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+    lucid $ do
+      pageTemplate "my title"
+      p_ $ do
+        "one more line"
+        p_ . toHtml $ "You are visit number " `T.append` (T.pack . show $ visitNum) `T.append` "!"
+        p_ "Please put your name in the guestbook"
+        p_ $ a_ [href_ "guestbook"] "GuestBook"
+  get "guestbook" $ do
+    guests <- runQuery (\x -> Pg.query_ x "select * from guest;" :: IO [Pg.Only T.Text])
+    lucid $ do
+      pageTemplate "my guestbook"
+      h1_ "Guest List"
+      guestTemplate guests
+      h1_ "Guest Sign In"
 
-helloAction :: SpockAction MyDb MySess MyState ()
-helloAction = do
-  helloInHtml
-
-helloInHtml :: SpockActionCtx ctx MyDb MySess MyState ()
-helloInHtml = lucid $ do
+pageTemplate :: T.Text -> Html ()
+pageTemplate title = do
   html_ $ do
-    head_ (title_ "Hello")
+    head_ (title_ $ toHtml title)
     body_ $ do
       h1_ "Hello!"
       p_ "Hello! Lucid"
@@ -44,6 +58,17 @@ helloInHtml = lucid $ do
         "I love "
         a_ [href_ "http://haskell.org"] "doggy"
         " !!!"
+
+guestTemplate :: [Pg.Only T.Text] -> Html ()
+guestTemplate xs = do
+  table_ $ do
+    tr_ $ do
+      th_ "Guest"
+    sequence_ $ map f xs
+      where f :: Monad m => Pg.Only T.Text -> HtmlT m ()
+            f (Pg.Only x) = do
+              tr_ $ do
+                td_ $ toHtml x
 
 createTable :: MyDb -> IO Int64
 createTable c =
